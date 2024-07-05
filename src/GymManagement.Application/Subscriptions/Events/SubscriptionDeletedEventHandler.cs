1d2242e2c12baf47aa9139ef47d6f9877ae2aefa
Yap Joon Lok <jl.yap@klk.com.my>
2024-07-05 10:02:05 +0800

    Create domain events when admin deletes a subscription (deletion not propagated to DB yet)

---
+++ src/GymManagement.Application/Subscriptions/Events/SubscriptionDeletedEventHandler.cs
@@ -0,0 +1,28 @@
+﻿using GymManagement.Application.Common.Interfaces;
+using GymManagement.Domain.Admins.Events;
+using MediatR;
+
+namespace GymManagement.Application.Subscriptions.Events;
+
+public class SubscriptionDeletedEventHandler : INotificationHandler<SubscriptionDeletedEvent>
+{
+    private readonly ISubscriptionsRepository _subscriptionsRepository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public SubscriptionDeletedEventHandler(
+        ISubscriptionsRepository subscriptionsRepository,
+        IUnitOfWork unitOfWork)
+    {
+        _subscriptionsRepository = subscriptionsRepository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public async Task Handle(SubscriptionDeletedEvent notification, CancellationToken cancellationToken)
+    {
+        var subscription = await _subscriptionsRepository.GetByIdAsync(notification.SubscriptionId)
+            ?? throw new InvalidOperationException();
+
+        await _subscriptionsRepository.RemoveSubscriptionAsync(subscription);
+        await _unitOfWork.CommitChangesAsync();
+    }
+}
